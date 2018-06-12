e901052ee46c777b76b0ce5bea1a6f313e530f91
Fan Yang <cedric.yf@gmail.com>
2018-06-12 18:43:20 -0400

    fix: product and category use different route

--- src/api/home_products.js
+++ src/api/home_products.js
@@ -4,6 +4,6 @@ const API = `${process.env.PUBLIC_URL}home/products`
 
 export const index = () => { return axios.get(API) }
 
-export const indexCategory = (params) => { return axios.get(`${API}/${params.category_id}`)}
+export const indexCategory = (params) => { return axios.get(`${API}/category/${params.category_id}`)}
 
 export const show = (params) => { return axios.get(`${API}/${params.product_id}`) }
