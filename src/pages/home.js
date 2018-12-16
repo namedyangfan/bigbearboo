a1cddb30a8099d53a58708d7b7fa0199e7561173
Fan Yang <cedric.yf@gmail.com>
2018-12-16 11:26:02 -0500

    fix: remove dummy functions

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,25 +4,12 @@ import ShopItem from 'components/home/shop_item'
 import SlickCarousel from 'components/home/slick_carousel'
 
 export default class Home extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      numShopItem: 0
-    };
-  }
-
-  handleAddShopItem = () => {
-    console.log(this.state.numShopItem)
-    this.setState((prevState) => {
-      return {numShopItem: prevState.numShopItem + 1}
-    })
-  }
 
   render() {
     return (
       <div className='grey lighten-4'>
         <SlickCarousel />
-        <ShopItem handleClick={this.handleAddShopItem} history={this.props.history}/>
+        <ShopItem history={this.props.history}/>
       </div>
       );
   }
