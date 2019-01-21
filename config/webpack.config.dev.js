a6129a90d5e4e5d34fe1702500db3c6b1c6b6dfb
Fan Yang <cedric.yf@gmail.com>
2019-01-21 17:23:56 -0500

    feat: api request point to heroku

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -18,7 +18,8 @@ const publicPath = '/';
 // `publicUrl` is just like `publicPath`, but we will provide it to our app
 // as %PUBLIC_URL% in `index.html` and `process.env.PUBLIC_URL` in JavaScript.
 // Omit trailing slash as %PUBLIC_PATH%/xyz looks better than %PUBLIC_PATH%xyz.
-const publicUrl = 'http://0.0.0.0:3000/';
+// const publicUrl = 'http://0.0.0.0:3000/';
+const publicUrl = 'https://cryptic-garden-52827.herokuapp.com/';
 // Get environment variables to inject into our app.
 const env = getClientEnvironment(publicUrl);
 
